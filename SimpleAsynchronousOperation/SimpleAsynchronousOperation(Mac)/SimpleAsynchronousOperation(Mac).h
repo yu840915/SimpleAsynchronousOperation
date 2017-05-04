42b92c56f05a045aeaa915292a4f887bcea7d799
yu840915 <mike@17.media>
2017-05-04 17:10:29 +0800

    Add mac target

---
+++ SimpleAsynchronousOperation/SimpleAsynchronousOperation(Mac)/SimpleAsynchronousOperation(Mac).h
@@ -0,0 +1,19 @@
+//
+//  SimpleAsynchronousOperation(Mac).h
+//  SimpleAsynchronousOperation(Mac)
+//
+//  Created by Mike Yu on 04/05/2017.
+//  Copyright © 2017 small. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SimpleAsynchronousOperation(Mac).
+FOUNDATION_EXPORT double SimpleAsynchronousOperation_Mac_VersionNumber;
+
+//! Project version string for SimpleAsynchronousOperation(Mac).
+FOUNDATION_EXPORT const unsigned char SimpleAsynchronousOperation_Mac_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleAsynchronousOperation_Mac_/PublicHeader.h>
+
+
